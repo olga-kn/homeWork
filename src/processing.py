fa040869f6b87ddf1611b51e93d13de6e6cff5a0
ольга кривоногова <26olga.k@gmail.com>
2025-07-14 23:28:51 +0500

    add sort by date funcktion

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,20 @@
-def filter_by_state(data_list: list, state: str = 'EXECUTED') -> list:
-    """ Функция фильтрации по статусу операции """
+from datetime import datetime
+from typing import Dict
+from typing import List
+
+
+def filter_by_state(data_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция фильтрации по статусу операции"""
     result_list = []
     for dict in data_list:
-        if dict.get('state') == state:
+        if dict.get("state") == state:
             result_list.append(dict)
         else:
-                continue
+            continue
     return result_list
 
+
+def sort_by_date(list_of_dict: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
+    """Функция фильтрации по дате опрерации"""
+    sorted_list_of_dict = sorted(list_of_dict, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
+    return sorted_list_of_dict
