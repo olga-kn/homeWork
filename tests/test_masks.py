b495373c4886dd478a7c8aa8ebf501522dd1bb3b
ольга кривоногова <26olga.k@gmail.com>
2025-07-23 23:25:37 +0500

    fix test masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,13 +6,7 @@ from src.masks import get_mask_card_number
 
 # Функция get_mask_card_number
 # Тестирование правильности маскирования номера карты.
-@pytest.fixture
-def number():
-    return "1478523697412365"
-
-
-def test_get_mask_card_number(number):
-    assert get_mask_card_number(number) == "1478 52** **** 2365"
+assert get_mask_card_number("1478523697412365") == "1478 52** **** 2365"
 
 
 # Проверка работы функции на различных входных форматах номеров карт, включая нестандартные длины номеров и отсутствие номера карты.
@@ -31,13 +25,7 @@ def test_get_mask_card_number(card_number, result):
 
 # Функция get_mask_account
 # Тестирование правильности маскирования номера счета.
-@pytest.fixture
-def num():
-    return "23558766748914578546"
-
-
-def test_get_mask_account(num):
-    assert get_mask_account(num) == "**8546"
+assert get_mask_account("23558766748914578546") == "**8546"
 
 
 # Проверка работы функции с различными форматами и длинами номеров счетов.
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,6 @@
 # import pytest
 #
+#
 # from src.processing import filter_by_state, sort_by_date
 
 
